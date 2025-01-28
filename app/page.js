3fd784ebae3b6b1d705348a75acf6a22dd713359
Mikethestrike23 <michael.lalonde1@edu.sait.ca>
2025-01-28 13:18:07 -0700

    Adding Link to Week 2 assignment

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
 import Image from "next/image";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>CPRG306: Web Development 2 - Assignments</h1>
-
-  )
+    <div>
+      <h1>CPRG306: Web Development 2 - Assignments</h1>
+        <Link href="/week-2">
+           <p> Week 2 Assignment </p>
+        </Link>
+    
+    </div>
+  );
 }
