fe74dd9ed1b5f077773a17216f864cac53db6922
Mikethestrike23 <michael.lalonde1@edu.sait.ca>
2025-02-25 20:56:21 -0700

    added basic functions and drop down list

---
+++ app/week-5/new-item.js
@@ -0,0 +1,46 @@
+'use client';
+import { useState } from "react";
+
+export default function NewItem() {
+  
+  const [quantity, setQuantity] = useState(1);
+
+  
+  const increment = () => {
+    if (quantity < 20) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+ 
+  const decrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  return (
+    <div className="p-3 shadow-md flex items-center justify-center space-x-1">
+        <div className="border-2 border-white p-3 rounded-lg">
+    
+        <div className="text-xl font-semibold">{quantity}</div>
+
+            <button className= "rounded-lg bg-red-700 hover:bg-red-500 active:bg-red-300 p-3"onClick={decrement} disabled={quantity === 1}> 
+          -
+            </button>
+        
+        
+            <button className="rounded-lg bg-emerald-900 hover:bg-emerald-500 active:bg-emerald-300 p-3"
+          onClick={increment}
+          disabled={quantity === 20}
+            >
+          +
+         </button>
+        </div>
+        
+    </div>
+   
+   
+);
+}
+
---
+++ app/week-5/page.js
@@ -0,0 +1,70 @@
+'use client';
+import { useState } from "react";
+import NewItem from "./new-item";
+import Item from "../week-3/item";
+
+export default function Page() {
+  
+  const [name, setName] = useState("");
+  const [category,setCategory] = useState("produce");
+  const handleSubmit=(Event)=>{
+    Event.preventDefault();
+    const item = {name, quantity, category};
+    console.log(item);
+    alert(`Name: ${name}, Quantity: ${quantity}, Category: ${category}`);
+    setName("");
+    setCategory("produce");
+  };
+  
+
+
+
+
+  
+
+
+
+  return (
+    <main>
+    <NewItem></NewItem>
+    
+    <label htmlFor="Itemname"></label><input id= "quantity" placeholder="Item Name" type="text">
+    </input>
+    <button type="submit" className="bg-white" onClick={handleSubmit}>submit</button>
+
+      <div>
+          <label htmlFor="category" className="block text-sm font-medium text-gray-700">Category</label>
+          <select
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}>
+            <option value="produce">Produce</option>
+            <option value="dairy">Dairy</option>
+            <option value="bakery">Bakery</option>
+            <option value="meat">Meat</option>
+            <option value="frozen foods">Frozen Foods</option>
+            <option value="canned goods">Canned Goods</option>
+            <option value="dry goods">Dry Goods</option>
+            <option value="beverages">Beverages</option>
+            <option value="snacks">Snacks</option>
+            <option value="household">Household</option>
+            <option value="other">Other</option>
+          </select>
+      </div>
+  
+  </main>
+  )
+   
+
+  
+  
+
+
+
+
+
+
+
+}
+
+
